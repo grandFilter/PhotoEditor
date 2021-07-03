89240899517113129fd3934b5cc6dd25a171a719
imjeen <imjeen@sina.com>
2021-07-03 17:57:42 +0800

    :sparkles: add the basic grid

--- src/containers/Stage.tsx
+++ src/containers/Stage.tsx
@@ -1,6 +1,13 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 
-import { getStageSize } from "@/utils";
+import {
+  setupCanvas,
+  getStageSize,
+  drawGrid,
+  drawBackground,
+  drawAxes,
+  debounce,
+} from "@/utils";
 
 import styles from "./styles.module.less";
 
@@ -11,50 +18,42 @@ import styles from "./styles.module.less";
 export default function Stage() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  const { width, height } = getStageSize();
+  const [size, setSize] = useState(getStageSize());
 
   useEffect(() => {
     const ctx = setupCanvas(canvasRef.current);
 
     if (!ctx) return;
 
-    ctx.lineWidth = 2;
-    ctx.beginPath();
-    ctx.moveTo(100, 100);
-    ctx.lineTo(200, 200);
-    ctx.stroke();
+    const { width, height } = size;
+
+    ctx.save();
+    ctx.fillStyle = "#fff";
+    ctx.fillRect(0, 0, width, height);
+    ctx.restore();
+
+    // drawGrid(ctx);
+    drawBackground(ctx);
+    drawAxes(ctx);
+  }, [size]);
+
+  useEffect(() => {
+    const handleResize = debounce(() => {
+      setSize(getStageSize());
+    }, 200);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
 
   return (
     <>
       <canvas
         ref={canvasRef}
-        style={{ width: `${width}px`, height: `${height}px` }}
+        style={{ width: `${size.width}px`, height: `${size.height}px` }}
         className={styles.stage}
       />
     </>
   );
-}
-
-/**
- * High DPI Canvas
- *
- * @param {(HTMLCanvasElement | null)} canvas - canvas元素
- * @return {CanvasRenderingContext2D | null}
- */
-function setupCanvas(
-  canvas: HTMLCanvasElement | null
-): CanvasRenderingContext2D | null {
-  if (!canvas) return null;
-
-  const DPR = window.devicePixelRatio || 1;
-
-  const rect = canvas.getBoundingClientRect();
-  canvas.width = rect.width * DPR;
-  canvas.height = rect.height * DPR;
-  const ctx = canvas.getContext("2d");
-
-  ctx?.scale(DPR, DPR);
-
-  return ctx;
 }
